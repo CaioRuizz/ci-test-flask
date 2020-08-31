7626b5c10ece4da1edc032ea7c8859f6db004a1c
CaioRuizz <caio.ruiz00@gmail.com>
2020-08-31 16:07:53 -0300

    adding project

---
+++ tests/__main__.py
@@ -0,0 +1,5 @@
+import unittest
+from .testloader import *
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ tests/testloader.py
@@ -0,0 +1,12 @@
+from app import app
+
+import unittest
+
+class TestApp(unittest.TestCase):
+    """Unit tests defined for app.py"""
+
+    def test_return_backwards_string(self):
+        """Test return backwards simple string"""
+        random_string = "This is my test string"
+        random_string_reversed = "gnirts tset ym si sihT"
+        self.assertEqual(random_string_reversed, app.returnBackwardsString(random_string))
